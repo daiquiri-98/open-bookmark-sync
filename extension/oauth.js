be723c43954b451f705fd09c31fe5c46a4dad2dd
daiquiri <github.tyf3h@alias.daiquiri.dev>
2025-09-28 10:24:12 +0300

    .

--- extension/oauth.js
+++ extension/oauth.js
@@ -5,7 +5,7 @@ class RaindropOAuth {
     this.AUTHORIZE_URL = 'https://raindrop.io/oauth/authorize';
     this.TOKEN_URL = 'https://raindrop.io/oauth/access_token';
     this.API_BASE = 'https://api.raindrop.io/rest/v1';
-    this.DEFAULT_MANAGED_BASE = 'https://login-with-raindrop.hello-a71.workers.dev';
+    this.DEFAULT_MANAGED_BASE = 'https://rdoauth.daiquiri.dev';
     this.MANAGED_ENABLED = true;
   }
 
